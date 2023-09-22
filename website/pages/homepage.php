e625f4b969fb4eba8127409880d61731e1e000e6
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-09-22 16:49:09 -0500

    remove accordian - did not work

--- website/pages/homepage.php
+++ website/pages/homepage.php
@@ -28,64 +28,8 @@
         // Get all Markdown files and folder structure
         $markdownFiles = scanDirectory('assets/markdown');
 
-        // Function to create a nested list of files and folders
-        function renderList($items, $basePath = '') {
-            $files = [];
-            $folders = [];
-
-            // Separate files and folders
-            foreach ($items as $item) {
-                if (is_array($item)) {
-                    $folders[] = $item;
-                } else {
-                    $files[] = $item;
-                }
-            }
-
-            echo '<div class="accordion" id="file-list">';
-
-            // Display folders first
-            foreach ($folders as $folder) {
-                echo '<div class="card">';
-                echo '<div class="card-header" id="folder-' . $folder['folder'] . '">';
-                echo '<h2 class="mb-0">';
-                echo '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#folder-' . $folder['folder'] . '-content" aria-expanded="true" aria-controls="folder-' . $folder['folder'] . '-content">';
-                echo $folder['folder'];
-                echo '</button>';
-                echo '</h2>';
-                echo '</div>';
-                echo '<div id="folder-' . $folder['folder'] . '-content" class="collapse" aria-labelledby="folder-' . $folder['folder'] . '" data-parent="#file-list">';
-                echo '<div class="card-body">';
-                renderList($folder['subFiles'], $basePath . $folder['folder'] . '/');
-                echo '</div>';
-                echo '</div>';
-                echo '</div>';
-            }
-
-            // Then display files
-            foreach ($files as $file) {
-                $relativePath = $basePath . $file;
-                echo '<div class="card">';
-                echo '<div class="card-header" id="file-' . $file . '">';
-                echo '<h2 class="mb-0">';
-                echo '<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#file-' . $file . '-content" aria-expanded="true" aria-controls="file-' . $file . '-content">';
-                echo $file;
-                echo '</button>';
-                echo '</h2>';
-                echo '</div>';
-                echo '<div id="file-' . $file . '-content" class="collapse" aria-labelledby="file-' . $file . '" data-parent="#file-list">';
-                echo '<div class="card-body">';
-                echo '<p>Content of ' . $file . ' goes here.</p>';
-                echo '</div>';
-                echo '</div>';
-                echo '</div>';
-            }
-
-            echo '</div>';
-        }
-
         // Function to render a nested list
-        function renderListOLD($items, $basePath = '') {
+        function renderListTree($items, $basePath = '') {
             $files = [];
             $folders = [];
         
@@ -109,14 +53,14 @@
             // Then display folders
             foreach ($folders as $folder) {
                 echo '<li><strong>' . $folder['folder'] . '</strong>';
-                renderList($folder['subFiles'], $basePath . $folder['folder'] . '/');
+                renderListTree($folder['subFiles'], $basePath . $folder['folder'] . '/');
                 echo '</li>';
             }
         
             echo '</ul>';
         }
                 // Display the nested list
-        renderList($markdownFiles);
+        renderListTree($markdownFiles);
         ?>
     </ul>
     </div>
