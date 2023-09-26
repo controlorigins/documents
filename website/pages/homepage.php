95c87ecf15cb5668284cf7a3a6b925469dbb3665
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-09-26 09:02:42 -0500

    change to drop down for file list

--- website/pages/homepage.php
+++ website/pages/homepage.php
@@ -1,89 +1,102 @@
 <h1>Documentation</h1>
 <div class="row">
-    <div class="col-3">
-    <ul>
-        <?php
-        // Function to recursively scan a directory and return an array of file paths
-        function scanDirectory($dir) {
-            $files = [];
-            foreach (scandir($dir) as $file) {
-                if ($file !== '.' && $file !== '..') {
-                    $filePath = $dir . '/' . $file;
-                    if (is_dir($filePath)) {
-                        $subFiles = scanDirectory($filePath);
-                        if (!empty($subFiles)) {
-                            $files[] = [
-                                'folder' => $file,
-                                'subFiles' => $subFiles,
-                            ];
+    <div class="col-12">
+        <form action="" method="GET">
+            <div class="form-group">
+                <label for="fileSelect"><h2>Select a file:</h2></label>
+                <select class="form-control" name="file" id="fileSelect">
+                    <?php
+                    // Function to recursively scan a directory and return an array of file paths
+                    function scanDirectory($dir, $basePath = '') {
+                        $files = [];
+                        $subFiles = [];
+
+                        foreach (scandir($dir) as $file) {
+                            if ($file !== '.' && $file !== '..') {
+                                $filePath = $dir . '/' . $file;
+                                $relativePath = $basePath . $file;
+                                if (is_dir($filePath)) {
+                                    $subFiles = array_merge($subFiles, scanDirectory($filePath, $relativePath . '/'));
+                                } elseif (pathinfo($file, PATHINFO_EXTENSION) === 'md') {
+                                    $files[] = $relativePath;
+                                }
+                            }
                         }
-                    } elseif (pathinfo($file, PATHINFO_EXTENSION) === 'md') {
-                        $files[] = $file;
+
+                        return array_merge($files, $subFiles);
                     }
-                }
-            }
-            return $files;
-        }
 
-        // Get all Markdown files and folder structure
-        $markdownFiles = scanDirectory('assets/markdown');
+                    // Get all Markdown files and folder structure
+                    $markdownFiles = scanDirectory('assets/markdown');
 
-        // Function to render a nested list
-        function renderListTree($items, $basePath = '') {
-            $files = [];
-            $folders = [];
+                    // Function to render options for the dropdown
+                    function renderDropdownOptions($items) {
+                        $options = '';
+
+                        foreach ($items as $item) {
+                            if (is_array($item)) {
+                                $folderName = $item['folder'];
+                                $subOptions = renderDropdownOptions($item['subFiles']);
+                                if (!empty($subOptions)) {
+                                    $options .= '<optgroup label="' . $folderName . '">' . $subOptions . '</optgroup>';
+                                }
+                            } else {
+                                $encodedPath = urlencode($item);
+                                // Check if this option should be selected
+                                $selected = ($_GET['file'] === $encodedPath || (!isset($_GET['file']) && $item === reset($items))) ? 'selected' : '';
+                                $options .= '<option value="' . $encodedPath . '" ' . $selected . '>' . $item . '</option>';
+                            }
+                        }
+
+                        return $options;
+                    }
+
+                    // Render options for the dropdown
+                    echo renderDropdownOptions($markdownFiles);
+                    ?>
+                </select>
+            </div>
+        </form>
         
-            // Separate files and folders
-            foreach ($items as $item) {
-                if (is_array($item)) {
-                    $folders[] = $item;
+        <div class="document-display">
+            <?php
+            // Display the selected Markdown file in HTML
+            if (isset($_GET['file']) || (isset($markdownFiles[0]) && !isset($_GET['file']))) {
+                $requestedFile = 'assets/markdown/' . urldecode($_GET['file'] ?? $markdownFiles[0]); // Decode the URL parameter or use the default
+                if (file_exists($requestedFile) && pathinfo($requestedFile, PATHINFO_EXTENSION) === 'md') {
+                    // Include the Parsedown library
+                    require_once('pages/Parsedown.php');
+
+                    $markdownContent = file_get_contents($requestedFile);
+                    // Create a new Parsedown instance
+                    $parsedown = new Parsedown();
+                    // Parse Markdown to HTML and display it
+                    echo $parsedown->text($markdownContent); // Corrected variable name
                 } else {
-                    $files[] = $item;
+                    echo 'File not found or invalid file format.';
                 }
             }
-        
-            echo '<ul>';
-            
-            // Display files first
-            foreach ($files as $file) {
-                $relativePath = $basePath . $file;
-                echo '<li><a href="?file=' . urlencode($relativePath) . '">' . $file . '</a></li>';
-            }
-            
-            // Then display folders
-            foreach ($folders as $folder) {
-                echo '<li><strong>' . $folder['folder'] . '</strong>';
-                renderListTree($folder['subFiles'], $basePath . $folder['folder'] . '/');
-                echo '</li>';
-            }
-        
-            echo '</ul>';
-        }
-                // Display the nested list
-        renderListTree($markdownFiles);
-        ?>
-    </ul>
-    </div>
-    <div class="col-9">
-    <?php
-        // Display the selected Markdown file in HTML
-        if (isset($_GET['file'])) {
-            $requestedFile = 'assets/markdown/' . $_GET['file'];
-            if (file_exists($requestedFile) && pathinfo($requestedFile, PATHINFO_EXTENSION) === 'md') {
-                
-                // Include the Parsedown library
-                require_once('pages/Parsedown.php');
-
-                $markdownContent = file_get_contents($requestedFile);
-                // Create a new Parsedown instance
-                $parsedown = new Parsedown();
-                // Parse Markdown to HTML and display it
-                echo $parsedown->text($markdownContent); // Corrected variable name
-                
-            } else {
-                echo 'File not found or invalid file format.';
-            }
-        }
-        ?>
+            ?>
+        </div>
     </div>
 </div>
+
+<script>
+    // JavaScript to automatically load the document when the select changes
+    const fileSelect = document.getElementById('fileSelect');
+    fileSelect.addEventListener('change', () => {
+        const selectedFile = fileSelect.value;
+        // Redirect to the selected file
+        window.location.href = `?file=${encodeURIComponent(selectedFile)}`;
+    });
+</script>
+
+<script>
+    // JavaScript to automatically load the document when the select changes
+    const fileSelect = document.getElementById('fileSelect');
+    fileSelect.addEventListener('change', () => {
+        const selectedFile = fileSelect.value;
+        // Redirect to the selected file
+        window.location.href = `?file=${encodeURIComponent(selectedFile)}`;
+    });
+</script>
