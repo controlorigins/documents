5008c53890ba8fbd11e694c487da265934be94cb
Mark Hazleton <mark.hazleton@controlorigins.com>
2023-10-03 08:21:51 -0500

    On file not found, default to document view

--- website/index.php
+++ website/index.php
@@ -9,7 +9,7 @@ $page = isset($_GET['page']) ? (string)$_GET['page'] : 'document_view';
 // Check if the requested page exists in the content
 if (!array_key_exists($page, $content) || !isset($content[$page]['title']) || !isset($content[$page]['content'])) {
     // Default to a 404 page if the page doesn't exist or lacks title/content
-    $page = '404';
+    $page = 'document_view';
     $content[$page]['title'] = '404 - Not Found';
     $content[$page]['content'] = 'Page Not Found';
 }
